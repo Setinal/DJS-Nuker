6cb3d7ed27763997db58e31740344552d5d1a9d1
Setinal <66276697+Setinal@users.noreply.github.com>
2020-10-20 08:03:07 -0700

    Fixed something
    yx

--- commands/deletechannels.js
+++ commands/deletechannels.js
@@ -5,6 +5,7 @@ const ownerids = require('../ownerids.json');
 module.exports.run = async(bot, message, args) => {
   
   if(!ownerids.ownerid.includes(message.author.id)) return;
+
   
   message.guild.channels.forEach(message => {
   
